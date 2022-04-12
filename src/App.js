5d04751ad3a8c2e738f086fc36d28eb7e954b28e
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-12 09:09:54 -0500

    fixed api server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/static/NavBar";
 import Home from "./components/static/Home";
 import ArticleList from "./components/ArticleList";
+import { API_KEY } from "./Globals";
 
 function App() {
+  // make API call
+  const [articles, setArticles] = useState([]);
+  const apiUrl = `https://newsdata.io/api/1/news?apikey=${API_KEY}&country=us,ca,gb`;
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setArticles(data);
+    };
+    fetchArticles();
+  }, []);
+  console.log("articles", articles);
+
   return (
     <div className="App">
       <Router>
