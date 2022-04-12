0f1bb02ca427da3bed8c22ae79824add844d7753
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-12 09:17:38 -0500

    created article card

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { React, useState, useEffect } from "react";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/static/NavBar";
 import Home from "./components/static/Home";
-import ArticleList from "./components/ArticleList";
+import ArticleList from "./components/articles/ArticleList";
 import { API_KEY } from "./Globals";
 
 function App() {
-  // make API call
   const [articles, setArticles] = useState([]);
   const apiUrl = `https://newsdata.io/api/1/news?apikey=${API_KEY}&country=us,ca,gb`;
 
@@ -15,11 +14,11 @@ function App() {
     const fetchArticles = async () => {
       const response = await fetch(apiUrl);
       const data = await response.json();
-      setArticles(data);
+      setArticles(data.results);
     };
     fetchArticles();
   }, []);
-  console.log("articles", articles);
+  // console.log("articles", articles[0].title);
 
   return (
     <div className="App">
