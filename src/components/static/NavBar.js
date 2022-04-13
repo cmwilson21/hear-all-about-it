53c261bdb5ea4e16bb922738a775b36ef598ba30
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2022-04-13 15:48:35 -0500

    added basic navbar ccs

--- src/components/static/NavBar.js
+++ src/components/static/NavBar.js
@@ -3,12 +3,13 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      <ul>
-        <li>
+    <div className="navbar">
+      <h3 className="title">Hear All About It</h3>
+      <ul className="nav-ul">
+        <li className="nav-li">
           <NavLink to="/">Home</NavLink>
         </li>
-        <li>
+        <li className="nav-li">
           <NavLink to="/news">News</NavLink>
         </li>
       </ul>
